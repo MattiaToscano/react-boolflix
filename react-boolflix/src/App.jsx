79fbbc71aeeb27fa54b0ff0902b8639a0fddcc74
MattiaToscano <mattia1997ct@gmail.com>
2025-05-20 12:26:42 +0200

    [UPD] Integrata API

--- react-boolflix/src/App.jsx
+++ react-boolflix/src/App.jsx
@@ -1,26 +1,41 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
+const apiKey = '631f087dc87791077683c9d753605830'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <header>
-        <h1>BoolFlix</h1>
-      </header>
-      <div className="container">
-        <div className="row">
-          <div className="col-12 col-md-8">
-            <input type="text" placeholder="Cerca un film" className="form-control" />
+  const [inputValue, setInputValue] = useState('')
+
+  const handleClick = () => {
+    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${inputValue}`)
+      .then((response) => {
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  useEffect(() => {
+    setInputValue
+
+    return (
+      <>
+        <header>
+          <h1>BoolFlix</h1>
+        </header>
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-md-8">
+              <input type="text" placeholder="Cerca un film" className="form-control" />
+            </div>
+            <div className="col-12 col-md-4">
+              <button className="btn btn-primary">Cerca</button>
+            </div>
           </div>
-          <div className="col-12 col-md-4">
-            <button className="btn btn-primary">Cerca</button>
-          </div>
-        </div>
-      </ div>
-    </>
-  )
-}
+        </ div>
+      </>
+    )
+  }
 
 export default App
