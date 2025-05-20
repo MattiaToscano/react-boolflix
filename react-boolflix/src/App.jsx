612a9d602cd085ec1a31f61351ebd9fb4f72dbdc
MattiaToscano <mattia1997ct@gmail.com>
2025-05-20 12:14:50 +0200

    [UPD] Creata struttura base

--- react-boolflix/src/App.jsx
+++ react-boolflix/src/App.jsx
@@ -6,7 +6,19 @@ function App() {
 
   return (
     <>
-
+      <header>
+        <h1>BoolFlix</h1>
+      </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-8">
+            <input type="text" placeholder="Cerca un film" className="form-control" />
+          </div>
+          <div className="col-12 col-md-4">
+            <button className="btn btn-primary">Cerca</button>
+          </div>
+        </div>
+      </ div>
     </>
   )
 }
