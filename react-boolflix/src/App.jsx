b26329f9794a6aa8284771286d844171ec4dd264
MattiaToscano <mattia1997ct@gmail.com>
2025-05-20 12:33:38 +0200

    [UPD] Fixata visualizzazione in console dei film

--- react-boolflix/src/App.jsx
+++ react-boolflix/src/App.jsx
@@ -6,36 +6,42 @@ const apiKey = '631f087dc87791077683c9d753605830'
 function App() {
 
   const [inputValue, setInputValue] = useState('')
+  const [movies, setMovies] = useState([])
 
   const handleClick = () => {
     axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${inputValue}`)
       .then((response) => {
+        console.log(response.data.results);
+        setMovies(response.data.results);
       })
       .catch((error) => {
         console.log(error)
       })
   }
 
-  useEffect(() => {
-    setInputValue
-
-    return (
-      <>
-        <header>
-          <h1>BoolFlix</h1>
-        </header>
-        <div className="container">
-          <div className="row">
-            <div className="col-12 col-md-8">
-              <input type="text" placeholder="Cerca un film" className="form-control" />
-            </div>
-            <div className="col-12 col-md-4">
-              <button className="btn btn-primary">Cerca</button>
-            </div>
+  return (
+    <>
+      <header>
+        <h1>BoolFlix</h1>
+      </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-8">
+            <input
+              type="text"
+              placeholder="Cerca un film"
+              className="form-control"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+            />
           </div>
-        </ div>
-      </>
-    )
-  }
+          <div className="col-12 col-md-4">
+            <button className="btn btn-primary" onClick={handleClick}>Cerca</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
 
 export default App
